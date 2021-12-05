f1c38c5a8a279d6474c60f5bfb76b03e68e8886a
Dzénetan Massart <37848836+Kidpaddleetcie@users.noreply.github.com>
2021-12-05 17:16:52 +0100

    Test d'affichage

--- assets/js/kg.js
+++ assets/js/kg.js
@@ -49,11 +49,11 @@ document.getElementById("ws").innerHTML = ws;
 
 
 // Mes chaînes YouTube
-/*
-                <a type="button" class="btn btn-outline-primary d-flex flex-wrap " target="_blank" href="https://www.youtube.com/channel/UCLCizq_irSximVxMTpx9-zA/videos">LesYTPs2Glin</a>
-                <a type="button" class="btn btn-outline-primary d-flex flex-wrap" target="_blank" href="https://www.youtube.com/channel/UC_akJUSyHI0UTm6wa7nWwnA/videos">Délires2Glin</a>
-                <a type="button" class="btn btn-outline-primary d-flex flex-wrap" target="_blank" href="https://www.youtube.com/channel/UCYWtLWor1OLvHAfvh1Ttj7Q/videos">Alphabet[FrenchYTP]</a>
-                <a type="button" class="btn btn-outline-primary d-flex flex-wrap" target="_blank" href="https://www.youtube.com/channel/UCwFC7lnTkK5zS9zzgBt81hw/videos">Ressources2Glin</a>
-                <a type="button" class="btn btn-outline-primary d-flex flex-wrap" target="_blank" href="https://www.youtube.com/channel/UCrbrtkq3QSeezauTuY7lsKw/videos">Tutos2Glin</a>
+let yt_list = ["UCLCizq_irSximVxMTpx9-zA", "UC_akJUSyHI0UTm6wa7nWwnA", "UCYWtLWor1OLvHAfvh1Ttj7Q", "UCwFC7lnTkK5zS9zzgBt81hw", "UCrbrtkq3QSeezauTuY7lsKw"]
+let yt = " ";
 
-*/
+for (let i = 0; i < yt_list.length; i++) {
+    let ytl = yt_list[i];
+    yt += '<div class="g-ytsubscribe" data-channelid="' + ytl + '" data-layout="full" data-count="default"></div>';
+}
+document.getElementById("yt").innerHTML = yt;
